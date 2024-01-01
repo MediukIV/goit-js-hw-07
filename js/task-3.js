0cf2902cc7468da5bfac310de3b0bc0e5fe42c89
MediukIV <143748068+MediukIV@users.noreply.github.com>
2024-01-01 22:27:03 +0200

    Add files via upload

---
+++ js/task-3.js
@@ -0,0 +1,11 @@
+const textInput = document.querySelector('#name-input');
+const textOutput = document.querySelector('#name-output');
+
+textInput.addEventListener('input', handleInputChange);
+
+function handleInputChange(e) {
+  textOutput.textContent = e.currentTarget.value;
+  if (e.currentTarget.value.trim() === '') {
+    textOutput.textContent = 'Anonymous';
+  }
+}
